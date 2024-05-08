0c62cdf79a8bfc9119da9e16c8e6957e48eeb516
Haroon Albar <haroonalbarx@gmail.com>
2024-05-09 02:32:38 +0530

    chore: formatted

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-func add (b []int, c chan int){
-  sum:= 0
-  for _, v:= range b{
-    sum += v
-  }
-  c <- sum
+func add(b []int, c chan int) {
+	sum := 0
+	for _, v := range b {
+		sum += v
+	}
+	c <- sum
 }
 
 func main() {
-  b:= []int{3,4,3,1,-5,2}
-  c:= make(chan int)
-  go add(b[:len(b)/2],c)
-  go add(b[len(b)/2:],c)
+	b := []int{3, 4, 3, 1, -5, 2}
+	c := make(chan int)
+	go add(b[:len(b)/2], c)
+	go add(b[len(b)/2:], c)
 
-  x,y:= <-c,<-c
+	x, y := <-c, <-c
 
-  fmt.Printf("x: %v, y:%v, x+y:%v \n",x,y,x+y)
+	fmt.Printf("x: %v, y:%v, x+y:%v", x, y, x+y)
 }
