60b148c788244eb26f65284893d231780d8bf3fa
Haroon Albar <haroonalbarx@gmail.com>
2024-05-09 02:15:56 +0530

    feat: use channel to add using 2 gorutines

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func add (b []int, c chan int){
+  sum:= 0
+  for _, v:= range b{
+    sum += v
+  }
+  c <- sum
+}
 
 func main() {
-	for i := 0; i < 10; i++ {
-		go fmt.Println(i)
-	}
-	time.Sleep(1 * time.Millisecond)
+  b:= []int{3,4,3,1,-5,2}
+  c:= make(chan int)
+  go add(b[:len(b)/2],c)
+  go add(b[len(b)/2:],c)
+
+  x,y:= <-c,<-c
+
+  fmt.Printf("x: %v, y:%v, x+y:%v \n",x,y,x+y)
 }
